ce7ad8c67b884a5ac5cd682db9406fc101b2d336
jrpespinas <jrpespinas@gmail.com>
2023-03-13 19:25:13 +0800

    src: upload Two Pointers Leetcode solutions

--- Leetcode Solutions/two-pointers/valid_palindrome.py
+++ Leetcode Solutions/two-pointers/valid_palindrome.py
@@ -20,3 +20,31 @@ def is_palindrome_2(s: str) -> bool:
     """
     s = "".join(char.lower() for char in s if char.isalnum())
     return s == s[::-1]
+
+
+def is_palindrome_3(s: str) -> bool:
+    """check if the given string is a palindrome
+
+    >>> is_palindrome_3("A man, a plan, a canal: Panama")
+    True
+    """
+    l, r = 0, len(s) - 1
+
+    while l < r:
+        while l < r and not is_alnum(s[l]):
+            l += 1
+
+        while r > l and not is_alnum(s[r]):
+            r -= 1
+
+        if s[l].lower() != s[r].lower():
+            return False
+
+        l += 1
+        r -= 1
+
+    return True
+
+
+def is_alnum(s: str) -> bool:
+    return "a" <= s.lower() <= "z" or "0" <= s.lower() <= "9"
